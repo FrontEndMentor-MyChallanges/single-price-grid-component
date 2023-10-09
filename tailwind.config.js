2f1505b93c4b6863f0c63e0a1fa78106435123cc
miroslaw-mroczkowski <mroczkowski.miroslaw@gmail.com>
2023-10-09 08:54:51 +0200

    Finished html and styles, updated readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  content: ['./**/*.{html,js}'],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#2ab2af',
-        primaryLight: '#43d3d0',
-        secondary: '	#c0df34',
-        light: '#e5eff5',
-        gray: '#98a6bd',
-      },
-      fontFamily: {
-        sans: ['"Karla"', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [],
+	content: ['./*.{html,js}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#2ab2af',
+				primaryLight: '#2dbebb',
+				secondary: '	#c0df34',
+				light: '#e5eff5',
+				gray: '#98a6bd',
+			},
+			fontFamily: {
+				sans: ['"Karla"', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [],
 };
